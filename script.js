ad66379ec65c9931fec8aaf245185598f4d8329f
Humaidi Hambali <aliesp241@gmail.com>
2023-03-31 11:15:02 +0700

    insert for loop #2

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-// FOR LOOP
+// FOR LOOP TASK 1
 for (let i = 1; i <= 100; i++){
     document.write('User ke - ' +i);
     document.write('<br/>');
+}
+
+// FOR LOOP TASK 2
+let nilai = 0;
+
+for (let i = 1; i <= 10; i++) {
+  nilai += 2;
+  console.log("Hasil penambahan ke-" + i + ": " + nilai);
 }
